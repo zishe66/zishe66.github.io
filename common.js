e95e0df65dc96e9a476a0883ba78c4c747306874
zishe66 <kerubi93@gmail.com>
2025-08-05 13:04:38 +0900

    Add files via upload

---
+++ common.js
@@ -0,0 +1,43 @@
+$(function(){
+  var pageTop = $("#page-top");
+  pageTop.hide();
+  pageTop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+  $(window).scroll(function () { 
+    if($(this).scrollTop() >= 200) {
+      pageTop.fadeIn();
+    } else {
+      pageTop.fadeOut();
+    }
+  });
+});
+
+
+// モダール部分
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modalImage');
+const imgs = document.querySelectorAll('.popup');
+const closeSpan = document.getElementById('close');
+
+for( let img of imgs) {
+    img.onclick = function(){
+      modal.style.opacity = "1";
+      modal.style.visibility = "visible";
+      modalImg.src = this.src;
+    }
+}
+
+// モダールを閉じる
+closeSpan.onclick = function() {
+    modal.style.opacity = "0";
+    modal.style.visibility = "hidden";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.opacity = "0";
+        modal.style.visibility = "hidden";
+    }
+}
